1ff90e2ced1f24fedd6cb333075547047faff63d
Anish Surti <srtanish1992@gmail.com>
2018-01-25 16:48:07 -0600

    added the project

---
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Typescript: The Good Parts', pages : 40 },
+    {title: 'Harry Potter', pages : 100},
+    {title: 'The Dark Tower', pages : 40},
+    {title: 'Eloquent Ruby', pages : 10}
+  ]
+}
